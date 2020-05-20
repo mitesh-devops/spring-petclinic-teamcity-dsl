cf43d1295f914bd013ad45b41c60f330263d53dc
mitesh-devops <58689332+mitesh-devops@users.noreply.github.com>
2020-05-20 10:50:34 +0530

    Update settings.kts

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -1,68 +1,64 @@
-import jetbrains.buildServer.configs.kotlin.v2018_2.*
-import jetbrains.buildServer.configs.kotlin.v2018_2.buildFeatures.Swabra
-import jetbrains.buildServer.configs.kotlin.v2018_2.buildFeatures.swabra
-import jetbrains.buildServer.configs.kotlin.v2018_2.buildSteps.maven
-import jetbrains.buildServer.configs.kotlin.v2018_2.triggers.vcs
-import jetbrains.buildServer.configs.kotlin.v2018_2.vcs.GitVcsRoot
+package _Self.buildTypes
 
-/*
-The settings script is an entry point for defining a TeamCity
-project hierarchy. The script should contain a single call to the
-project() function with a Project instance or an init function as
-an argument.
+import jetbrains.buildServer.configs.kotlin.v2019_2.*
+import jetbrains.buildServer.configs.kotlin.v2019_2.buildFeatures.commitStatusPublisher
+import jetbrains.buildServer.configs.kotlin.v2019_2.buildFeatures.pullRequests
+import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.script
+import jetbrains.buildServer.configs.kotlin.v2019_2.triggers.vcs
 
-VcsRoots, BuildTypes, Templates, and subprojects can be
-registered inside the project using the vcsRoot(), buildType(),
-template(), and subProject() methods respectively.
+object ExpressApp : BuildType({
+    name = "Express App"
 
-To debug settings scripts in command-line, run the
+    artifactRules = "coverage/** => coverage.zip"
 
-    mvnDebug org.jetbrains.teamcity:teamcity-configs-maven-plugin:generate
-
-command and attach your debugger to the port 8000.
-
-To debug in IntelliJ Idea, open the 'Maven Projects' tool window (View
--> Tool Windows -> Maven Projects), find the generate task node
-(Plugins -> teamcity-configs -> teamcity-configs:generate), the
-'Debug' option is available in the context menu for the task.
-*/
-
-version = "2018.2"
-
-project {
-    vcsRoot(PetclinicVcs)
-    buildType(Build)
-}
-
-object Build : BuildType({
-    name = "Build"
-    artifactRules = "target/*jar"
-
+    params {
+        param("build", "%teamcity.build.branch%-%build.counter%")
+        param("teamcity.vcsTrigger.runBuildInNewEmptyBranch", "false")
+    }
     vcs {
-        root(PetclinicVcs)
+        root(Cucumber)
     }
     steps {
-        maven {
-            goals = "clean package"
-            dockerImage = "maven:3.6.0-jdk-8"
+        step {
+            name = "Run Test"
+            type = "jonnyzzz.npm"
+            param("npm_commands", """
+                install
+                test
+            """.trimIndent())
+        }
+        script {
+            name = "testing"
+            scriptContent = """
+                npm install cucumber-teamcity-formatter --save-dev
+                ./node_modules/.bin/cucumber-js --format node_modules/cucumber-teamcity-formatter
+            """.trimIndent()
         }
     }
+
     triggers {
         vcs {
-            groupCheckinsByCommitter = true
+            perCheckinTriggering = true
+            enableQueueOptimization = false
+        }
+    }
+
+    features {
+        pullRequests {
+            enabled = false
+            vcsRootExtId = "${HttpsGitlabComMiteshITadcornerTestingProjectGitRefsHeadsMaster.id}"
+            provider = gitlab {
+                authType = token {
+                    token = "zxxddf13904f10b58a7d73448df922d7ee3b49df7f62ca5787d"
+                }
+            }
+        }
+        commitStatusPublisher {
+            vcsRootExtId = "${HttpsGitlabComMiteshITadcornerTestingProjectGitRefsHeadsMaster.id}"
+            publisher = gitlab {
+                gitlabApiUrl = "https://gitlab.com/api/v4"
+                accessToken = "zxxddf13904f10b58a7d73448df922d7ee3b49df7f62ca5787d"
+            }
         }
     }
 })
-
-object PetclinicVcs : GitVcsRoot({
-    name = "PetclinicVcs"
-    url = "https://github.com/spring-projects/spring-petclinic.git"
-})
-
-
-fun wrapWithFeature(buildType: BuildType, featureBlock: BuildFeatures.() -> Unit): BuildType {
-    buildType.features {
-        featureBlock()
-    }
-    return buildType
-}
